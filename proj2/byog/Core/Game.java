c913f44f9dcbc857107dda0030ec4dca41f1f96a
JinxinXiong <1165216941@qq.com>
2021-08-24 10:59:31 +0800

    proj2 phase1

--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -3,12 +3,14 @@ package byog.Core;
 import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
 
+import java.io.File;
+
 public class Game {
     TERenderer ter = new TERenderer();
     /* Feel free to change the width and height. */
     public static final int WIDTH = 80;
     public static final int HEIGHT = 30;
-
+    public static final File CWD = new File(System.getProperty("user.dir"));
     /**
      * Method used for playing a fresh game. The game should start from the main menu.
      */
@@ -27,12 +29,69 @@ public class Game {
      * @param input the input string to feed to your program
      * @return the 2D TETile[][] representing the state of the world
      */
-    public TETile[][] playWithInputString(String input) {
+    public static TETile[][] playWithInputString(String input) {
         // TODO: Fill out this method to run the game using the input passed in,
         // and return a 2D tile representation of the world that would have been
         // drawn if the same inputs had been given to playWithKeyboard().
-
+        InputSource inputSource = new StringInputSource(input);
+        StringBuilder s = new StringBuilder();
         TETile[][] finalWorldFrame = null;
+        boolean setSeed = false;
+        while (inputSource.possibleNextInput()) {
+            char c = inputSource.getNextKey();
+            if (c == 'N') {
+                System.out.println("N (New Game)");
+            } else if (c == 'S') {
+                if (setSeed) {
+                    System.out.println("S (Set Seed)");
+                } else {
+                    setSeed = true;
+                    long seed = seedFromString(s);
+                    finalWorldFrame = getRandomFrame(seed);
+                }
+            } else if (c == ':') {
+                if (inputSource.possibleNextInput()) {
+                    c = inputSource.getNextKey();
+                    if (c == 'q') {
+                        Utils.writeObject(Utils.join(CWD, "saved game"), finalWorldFrame);
+                        break;
+                    }
+                }
+            } else if ('0' <= c  && c <= '9'){
+                s.append(c);
+            } else if (c == 'l') {
+                finalWorldFrame = Utils.readObject(Utils.join(CWD, "saved game"), TETile[][].class);
+            } else {
+                System.out.println(c);
+            }
+        }
         return finalWorldFrame;
+    }
+
+    public static TETile[][] getRandomFrame(long s) {
+        RandomWorld.setSeed(s);
+
+        TETile[][] finalWorldFrame = new TETile[WIDTH][HEIGHT];
+        RandomWorld.fillWithRandomTiles(finalWorldFrame);
+        return finalWorldFrame;
+    }
+
+    public static long seedFromString(StringBuilder s) {
+        if (s.length() == 0) {
+            throw new IllegalArgumentException("Seed should be positive number!");
+        }
+        long seed = Long.parseLong(s.toString());
+        return seed;
+    }
+
+    public static void main(String[] args) {
+        TETile[][] world = playWithInputString("l");
+        int width = world.length;
+        int height = world[0].length;
+
+        TERenderer ter = new TERenderer();
+        ter.initialize(width, height);
+
+        ter.renderFrame(world);
     }
 }
---
+++ proj2/byog/Core/byogException.java
@@ -0,0 +1,11 @@
+package byog.Core;
+
+public class byogException extends RuntimeException{
+    byogException() {
+        super();
+    }
+
+    byogException(String msg) {
+        super(msg);
+    }
+}
